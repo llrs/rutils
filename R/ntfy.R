1b0f0bed924d3cd5befdac75648d35e1eddb009c
Lluís <lluis.revilla@gmail.com>
2023-08-27 13:14:24 +0200

    Fix the naming problems with ntfy

---
+++ R/ntfy.R
@@ -0,0 +1,22 @@
+# Use ntfy to send a message
+
+check_ntfy <- function() {
+  if (!requireNamespace("ntfy", quietly = TRUE)) {
+    stop("Install 'jonocarroll/ntfy' package")
+  }
+}
+
+#' Send a message to the NTFY address
+#'
+#' @param message Text to be sent.
+#' @param title Title of the notification.
+#' @param ... Other arguments used in `ntfy::ntfy_send()`
+#' @param topic  Send the message to the default NTFY environment variable:
+#' "NTFY_TOPIC". Specify topic if you don't use the default.
+#' @export
+#' @returns TRUE if sent.
+llrs_send_ntfy <- function(message, title, ..., topic  = NULL) {
+  check_ntfy()
+  topic <- if (is.null(topic)) ntfy::ntfy_topic() else topic
+  ntfy::ntfy_send(message, title, topic = topic, ...)
+}
