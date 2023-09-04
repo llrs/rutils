a41df35b11717c86c54b2e3124f2c4a7f8214305
Lluís Revilla <lrevilla@irsicaixa.es>
2023-09-04 16:59:43 +0200

    Document

--- R/git_hooks.R
+++ R/git_hooks.R
@@ -9,9 +9,13 @@
 #' @param path Path to the git repository
 #'
 #' @return A logical value if the hook can be placed there.
+#' @references <https://git-scm.com/docs/githooks>
+#' @seealso [llrs_hooks_list()]
 #' @export
+#' @examples
 #' \dontrun{
-#' llrs_hook("~/Documents/updates", "projects/git_something")
+#' hook <- system.file("post-receive", package = "rutils")
+#' llrs_hook(hook, "projects/git_something")
 #' }
 llrs_hook <- function(file, path = getwd()) {
   if (!check_git(path)) {
@@ -25,6 +29,7 @@ llrs_hook <- function(file, path = getwd()) {
   if (isFALSE(out)) {
     out
   }
+  Sys.chmod(hooks_path, mode = "0777")
   invisible(out)
 }
 
@@ -39,7 +44,11 @@ hooks_client <- c("pre-commit", "repare-commit-msg", "commit-msg", "post-commit"
                   "pre-push", "post-checkout",
                   "post-merge",
                   "pre-auto-gc")
-hooks_server <- c("pre-receive", "update", "post-receive")
+hooks_server <- c("pre-receive", "update", "proc-receive", "post-receive", "post-update",
+                  "reference-transaction", "push-to-checkout",
+                  "sendemail-validate", "fsmonitor-watchman",
+                  "p4-changelist", "p4-prepare-changelist", "p4-post-changelist", "p4-pre-submit",
+                  "post-index-change")
 
 hook_names <- function(path) {
   file <- basename(path)
@@ -54,7 +63,14 @@ hook_names <- function(path) {
   TRUE
 }
 
-# List existing hooks
+#' List existing hooks
+#'
+#' Check which hooks are in the package.
+#' @return A character vector with the name of the hooks.
+#' @export
+#' @seealso [llrs_hook()]
+#' @examples
+#' llrs_hooks_list()
 llrs_hooks_list <- function() {
   lf <- list.files("inst", full.names = FALSE)
   lf[lf %in% c(hooks_client, hooks_server)]
