c6a3f11f7b943877973c910bb06cc51deaaf1f5a
Lluís <lluis.revilla@gmail.com>
2024-11-02 13:04:45 +0100

    Also setup R environment

--- NEWS.md
+++ NEWS.md
@@ -3,6 +3,7 @@
  * Add functions to manage CNAG projects for `cellranger multi`.
  * Make `llrs_cellranger_clean()` more useful (keeping metrics).
  * Create folders just once in `llrs_cnag_symlinks()`
+ * Set up environment variables in `llrs_profile()`
  
 # rutils 0.0.1
 
--- R/set_profile.R
+++ R/set_profile.R
@@ -1,17 +1,36 @@
 #' Provide a R global user profile
 #'
-#' Write my preferences on a R user profile.
-#' @return Whatever [file.copy()] does
+#' Write my preferences on a R user profile and R environment.
+#' @return `TRUE` or `FALSE` depending on if it has overwritten the files or not.
 #' @export
-#' @importFrom utils menu
 llrs_profile <- function() {
-  dest_file <- "~/.Rprofile"
-  overwrite <- TRUE
-  if (file.exists(dest_file)) {
-    message("~/.Rprofile exists. Do you want to replace it?")
-    overwrite <- c(TRUE, FALSE)[menu(c("Yes", "No"))]
+  dest_file_profile <- "~/.Rprofile"
+  overwrite1 <- TRUE
+  if (file.exists(dest_file_profile)) {
+    overwrite1 <- askYesNo("~/.Rprofile exists. Do you want to replace it?",
+             default = "Cancel")
   }
   path <- base::system.file("r_profile", package = "rutils", mustWork = TRUE)
-  file.copy(path, to = dest_file, overwrite = overwrite)
-  message("Adjust the profile and restart the R session")
+  file.copy(path, to = dest_file_profile, overwrite = overwrite1)
+
+  dest_file_environ <- "~/.Renviron"
+  overwrite2 <- TRUE
+  if (file.exists(dest_file_environ)) {
+    overwrite2 <- askYesNo("~/.Renviron exists. Do you want to replace it?",
+             default = "Cancel")
+  }
+  path <- base::system.file("r_environ", package = "rutils", mustWork = TRUE)
+  file.copy(path, to = dest_file_environ, overwrite = overwrite2)
+
+  if (overwrite1 || overwrite2) {
+    if (overwrite1 && overwrite2 ) {
+      msg <- "R profile and environment"
+    } else if (overwrite1) {
+      msg <- "R profile"
+    } else if (overwrite2) {
+      msg <- "R environment"
+    }
+    message("Adjust the ", msg, " and restart the R session.")
+  }
+  overwrite1 || overwrite2
 }
