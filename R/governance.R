b3c8468e508c80e352eeecfd5d21d1a1cd39fe6d
Lluís <lluis.revilla@gmail.com>
2025-08-31 23:06:09 +0200

    Some functions

---
+++ R/governance.R
@@ -0,0 +1,35 @@
+llrs_use_governance <- function(path = ".") {
+  file_name <- "governance.md"
+  governance <- system.file("inst", file_name, package = "rutils")
+  path_out <- file.path(path, "governance.md")
+  if (dir.exists(path_out)) {
+    stop("There is a directory with this governance.md name")
+  }
+  out <- file.copy(governance, to = ".", overwrite = file.exists(path_out))
+  add2buildignore(file_name, path = path)
+  if (isTRUE(out) {
+    message("Added governance.md")
+  }
+  out
+}
+
+
+llrs_add_member <- function(name, role = "Collaborator", path = ".") {
+  role <- match.arg(role, c("Collaborator", "Publisher"))
+  file_name <- "members.csv"
+  out <- data.frame(Name = name,
+                    Role = role,
+                    `Date added` = as.character(Sys.Date()),
+                    `Date removed` = "",
+                    check.names = FALSE)
+  write.csv(out, append = TRUE, row.names = FALSE)
+}
+
+llrs_change_role <- function(name, role = "User", path = ".") {
+  role <- match.arg(role, c("User", "Collaborator", "Publisher"))
+  file_name <- "members.csv"
+  path_file <- file.path(path, file_name)
+  members <- read.csv(file = path_file, row.names = NULL, header = TRUE)
+
+  write.csv(out)
+}
---
+++ R/packages.R
@@ -0,0 +1,9 @@
+# Append to .Rbuildignore
+add2buildignore <- function(text, path = ".") {
+  file_name <- ".Rbuildignore"
+  path_file <- file.path(path, file_name)
+  if (!file.exists(path_file)) {
+    file.create(path_file)
+  }
+  cat(text, file = path_file, append = TRUE)
+}
